a23bbb5e520127a473b4fc1b62172ff8d9f1155f
Shakeel <shakeelkoper@gmail.com>
2022-05-16 20:09:59 +0530

    hash_util commit

---
+++ hash_util.py
@@ -0,0 +1,21 @@
+import json
+import hashlib as hl
+
+
+def hash_string_256(string):
+    """ Hashes a string with SHA256.
+
+    Arguments:
+        :string: The string that should be hashed.
+    """
+    return hl.sha256(string).hexdigest()
+
+
+def hash_block(block):
+    """ Hashes a block and returns a string representation of it.
+
+    Arguments:
+        :block: The block that should be hashed.
+    """
+    return hash_string_256(json.dumps(block, sort_keys=True).encode())
+    # return "-".join([str(block[key]) for key in block])
